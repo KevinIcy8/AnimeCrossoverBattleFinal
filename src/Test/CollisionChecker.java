3d2db134bbdb348b1f8db4278e9e655275b066bd
BT_4N2_23 <KevinIcy8>
2023-05-23 12:33:24 -0400

    added gravity system

--- src/Test/CollisionChecker.java
+++ src/Test/CollisionChecker.java
@@ -26,17 +26,18 @@ public class CollisionChecker {
             entity.collisionOn = true;
         }
 
-        entityBottomRow = (entityBottomWorldY + entity.speed)/gp.tileSize;
-        tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
-        tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
-        if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true){
-            entity.collisionOn = true;
-        }
+
         switch(entity.direction){
             case "up":
 
                 break;
             case "down":
+                entityBottomRow = (entityBottomWorldY + entity.speed)/gp.tileSize;
+                tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
+                tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
+                if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true){
+                    entity.collisionOn = true;
+                }
 
                 break;
             case "left":
